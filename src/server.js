ef65bc5bc2ab65e42d8a5ac44f84732868eeaeaa
pedrogodoy <pedrogh.godoy12@gmail.com>
2020-06-10 20:38:25 -0400

    Add modal - WIP

--- src/server.js
+++ src/server.js
@@ -7,6 +7,9 @@ const db = require("./database/db")
 //configurar pasta public para ser visivel na raiz do servidor
 server.use(express.static("public"))
 
+//Habilitar o uso do req.body
+server.use(express.urlencoded({ extended: true }))
+
 
 //Utilizando template engines
 const nunjucks = require("nunjucks")
@@ -22,6 +25,46 @@ server.get("/", (req, res) => {
 
 server.get("/create-point", (req, res) => {
     return res.render("create-point.html")
+})
+
+server.post("/savepoint", (req, res) => {
+
+    // inserir dados
+    const query = `
+        INSERT INTO places (
+            image, 
+            name,
+            address,
+            address2,
+            state,
+            city,
+            items
+        ) VALUES (?,?,?,?,?,?,?);
+    `
+    const values = [
+        req.body.image,
+        req.body.name,
+        req.body.address,
+        req.body.address2,
+        req.body.state,
+        req.body.city,
+        req.body.items
+
+    ]
+
+    function afterInsertData(err) {
+        if (err) {
+            return console.log(err)
+        }
+
+        console.log("Cadastrado com sucesso")
+        console.log(this)
+
+        return res.render("create-point.html", { saved: true })
+
+    }
+
+    db.run(query, values, afterInsertData) //Chamando funcao como referencia
 })
 
 server.get("/search", (req, res) => {
